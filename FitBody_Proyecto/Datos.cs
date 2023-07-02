07d2a9ba0c582be6569516ab5faa1b3912ccbf83
Eduardo <Eduardo@DESKTOP-90F1NSG>
2023-07-02 03:53:39 -0500

    Nuevos Datos

--- FitBody_Proyecto/Datos.cs
+++ FitBody_Proyecto/Datos.cs
@@ -6,19 +6,5 @@ namespace FitBody_Proyecto
 {
     internal class Datos
     {
-        public int codigo { get; set; }
-        public string calentamiento { get; set; }
-        public string cardio { get; set; }
-        public int tonificacion { get; set; }
-        public string piernas { get; set; }
-        public int estiramiento { get; set; }
-
-    }
-
-    class Ws
-    {
-        internal class Datos
-        {
-        }
     }
 }
