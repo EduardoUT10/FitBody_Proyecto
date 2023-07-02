ae31ac9b7ea91a4dffb2914d21ff094da57a3888
Eduardo <Eduardo@DESKTOP-90F1NSG>
2023-07-02 01:10:50 -0500

    Agregar archivos de proyecto.

---
+++ FitBody_Proyecto.Android/Datos.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FitBody_Proyecto.Droid
+{
+    internal class Datos
+    {
+        public int codigo { get; set; }
+        public string calentamiento { get; set; }
+        public string cardio { get; set; }
+        public int tonificacion { get; set; }
+        public string piernas { get; set; }
+        public int estiramiento { get; set; }
+    }
+}
---
+++ FitBody_Proyecto/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FitBody_Proyecto
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private const string Url = "http://192.168.1.6:8080/proyecto_fitbody/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<FitBody_Proyecto.Ws.Datos> _post;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(Url);
+            List<FitBody_Proyecto.Ws.Datos> posts = JsonConvert.DeserializeObject<List<FitBody_Proyecto.Ws.Datos>>(content);
+            _post = new ObservableCollection<FitBody_Proyecto.Ws.Datos>(posts);
+
+            MyListView.ItemsSource = posts;
+        }
+    }
+}
